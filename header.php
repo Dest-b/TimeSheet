dc5a8f35f5a2910d8e39d20b611bde390a17c3a4
Dest-b <qwerty36084@outlook.com>
2026-05-13 09:23:19 +0200

    the project

---
+++ header.php
@@ -0,0 +1,17 @@
+<?php require_once 'backend/config.php'; ?>
+<header>
+    <div class="container">
+        <nav>
+            <a href="<?php echo $base_url; ?>/index.php">Home</a> |
+            <a href="<?php echo $base_url; ?>/logs/index.php">Logs</a>
+        </nav>
+        <div>
+            <?php if (isset($_SESSION['user_id'])): ?>
+                <span>Hallo <?=htmlspecialchars($_SESSION['user_name'] ?? '')?></span> 
+                <a href="<?php echo $base_url; ?>/logout.php">Uitloggen</a>
+            <?php else: ?>
+                <a href="<?php echo $base_url; ?>/login.php">Inloggen</a>
+            <?php endif; ?>
+        </div>
+    </div>
+</header>
